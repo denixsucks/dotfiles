74983ca071004554eda157d8881db5b3b65947d2
denixsucks <erylmzdnz@gmail.com>
2023-01-20 19:05:56 +0300

    lol

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,3 +1,10 @@
 require 'config.nvim-tree'
 require 'config.feline'
 require 'config.nvim-cmp'
+require 'config.presence'
+require 'config.transparent'
+
+-- init some setups without config
+require('nvim_comment').setup()
+require('xresources')
+require("nvim-autopairs").setup {}
